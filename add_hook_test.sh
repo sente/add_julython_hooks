ea6612ce0c18e76b16927a572663bd12b87d39cd
Stuart Powers <stuart.powers@gmail.com>
2013-01-02 16:41:18 -0500

    added add_hook_test.sh

---
+++ add_hook_test.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# usage:
+# bash add_julython_hooks.sh USERNAME REPONAME
+
+# example:
+# bash add_julython_hooks.sh sente 
+#   this will add a hook to my http://github.com/sente/cloud9/ repo
+
+
+USERNAME=$1;
+REPO=$2;
+
+JSON=$(cat - <<"END"
+    {
+      "name": "web",
+      "active": true,
+      "events": [
+        "commit",
+        "push",
+        "pull_request"
+      ],
+      "config": {
+        "url": "http://sente.cc:7700",
+        "content_type": "json"
+      }
+    }
+END
+);
+
+curl -X POST -u "${USERNAME}" -d "$JSON" "https://api.github.com/repos/${USERNAME}/${REPO}/hooks"
+
+#why this isn't yet working, I don't know
+# testing..with   'nc -l 70000'
+
