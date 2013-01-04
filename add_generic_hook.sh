52698a96aa4d89bb99ac864db66fc89a734e5257
Stuart Powers <stuart.powers@gmail.com>
2013-01-04 01:00:05 -0500

    added a generic_hook

---
+++ add_generic_hook.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# usage:
+# bash add_julython_hooks.sh USERNAME REPONAME
+
+# example:
+# bash add_julython_hooks.sh sente cloud9
+#   this will add a hook to my http://github.com/sente/cloud9/ repo
+
+
+USERNAME=$1;
+REPO=$2;
+
+JSON=$(cat - <<"END"
+    {
+      "name": "web",
+      "active": true,
+      "events": [
+        "commit",
+        "push",
+        "pull_request"
+      ],
+      "config": {
+        "url": "http://sente.cc/git-hooks",
+        "content_type": "json",
+        "insecure_ssl": "1"
+      }
+    }
+END
+);
+
+curl -X POST -u "${USERNAME}" -d "$JSON" "https://api.github.com/repos/${USERNAME}/${REPO}/hooks"
+
+#why this isn't yet working, I don't know
+# testing..with   'nc -l 70000'
+
